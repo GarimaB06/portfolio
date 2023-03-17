0f62ab3a5b204e93dc0af6b23c990c8863409002
Garima Bhatia <marigolddancestudio@gmail.com>
2023-03-16 21:00:13 -0400

    This commit will add the recommendations section

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Home from './components/Home'
 import About from './components/About'
 import Skills from './components/Skills'
 import Work from './components/Work'
+import Testimonials from './components/Testimonials'
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
         <About />
         <Skills />
         <Work />
+        <Testimonials />
       </main>
     </div>
   )
