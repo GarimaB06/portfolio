1d774cef20097dab8189114cf7c5405069daa660
GarimaB06 <marigolddancestudio@gmail.com>
2023-08-06 13:29:26 -0400

    will add carousel view into bookcase and petparent components

--- src/components/BookCaseDemo.jsx
+++ src/components/BookCaseDemo.jsx
@@ -1,29 +1,51 @@
 import React from "react";
 import { BookcaseContent } from "./Content";
+import Flickity from "react-flickity-component";
+
+const flickityOptions = {
+	initialIndex: 0,
+};
 
 const BookCaseDemo = () => {
 	return (
 		<div className="bookcase-demo project-section">
-			<img
-				className="project-images"
-				src={BookcaseContent[0].image}
-				alt="bookcase-1"
-			/>
-			<img
-				className="project-images"
-				src={BookcaseContent[1].image}
-				alt="bookcase-2"
-			/>
-			<img
-				className="project-images"
-				src={BookcaseContent[2].image}
-				alt="bookcase-3"
-			/>
-			<img
-				className="project-images"
-				src={BookcaseContent[3].image}
-				alt="bookcase-4"
-			/>
+			<Flickity
+				className={"carousel"} // default ''
+				elementType={"div"} // default 'div'
+				options={flickityOptions} // takes flickity options {}
+				disableImagesLoaded={false} // default false
+				reloadOnUpdate // default false
+				static // default false
+			>
+				<div className="carousel-item-wrapper">
+					<img
+						className="project-images"
+						src={BookcaseContent[0].image}
+						alt="bookcase-1"
+					/>
+				</div>
+				<div className="carousel-item-wrapper">
+					<img
+						className="project-images"
+						src={BookcaseContent[1].image}
+						alt="bookcase-2"
+					/>
+				</div>
+				<div className="carousel-item-wrapper">
+					<img
+						className="project-images"
+						src={BookcaseContent[2].image}
+						alt="bookcase-3"
+					/>
+				</div>
+				<div className="carousel-item-wrapper">
+					<img
+						className="project-images"
+						src={BookcaseContent[3].image}
+						alt="bookcase-4"
+					/>
+				</div>
+			</Flickity>
 		</div>
 	);
 };
--- src/components/PetParentDemo.jsx
+++ src/components/PetParentDemo.jsx
@@ -1,29 +1,52 @@
 import React from "react";
 import { PetParentContent } from "./Content";
+import Flickity from "react-flickity-component";
+import "../styles/projects-demo.scss";
+
+const flickityOptions = {
+	initialIndex: 0,
+};
 
 const PetParentDemo = () => {
 	return (
 		<div className="petparent-demo project-section">
-			<img
-				className="project-images"
-				src={PetParentContent[0].image}
-				alt="petparent-1"
-			/>
-			<img
-				className="project-images"
-				src={PetParentContent[1].image}
-				alt="petparent-2"
-			/>
-			<img
-				className="project-images"
-				src={PetParentContent[2].image}
-				alt="petparent-3"
-			/>
-			<img
-				className="project-images"
-				src={PetParentContent[3].image}
-				alt="petparent-4"
-			/>
+			<Flickity
+				className={"carousel"} // default ''
+				elementType={"div"} // default 'div'
+				options={flickityOptions} // takes flickity options {}
+				disableImagesLoaded={false} // default false
+				reloadOnUpdate // default false
+				static // default false
+			>
+				<div className="carousel-item-wrapper">
+					<img
+						className="project-images"
+						src={PetParentContent[0].image}
+						alt="petparent-1"
+					/>
+				</div>
+				<div className="carousel-item-wrapper">
+					<img
+						className="project-images"
+						src={PetParentContent[1].image}
+						alt="petparent-2"
+					/>
+				</div>
+				<div className="carousel-item-wrapper">
+					<img
+						className="project-images"
+						src={PetParentContent[2].image}
+						alt="petparent-3"
+					/>
+				</div>
+				<div className="carousel-item-wrapper">
+					<img
+						className="project-images"
+						src={PetParentContent[3].image}
+						alt="petparent-4"
+					/>
+				</div>
+			</Flickity>
 		</div>
 	);
 };
