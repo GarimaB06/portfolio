5584028223c9f55c44d821caf4f2e840b9a6be86
GarimaB06 <marigolddancestudio@gmail.com>
2023-09-15 13:30:59 -0400

    will add useEffect to detect correct width and height of the device

--- src/components/BookCaseDemo.jsx
+++ src/components/BookCaseDemo.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { BookcaseContent } from "./Content";
 import Flickity from "react-flickity-component";
 
@@ -7,46 +7,107 @@ const flickityOptions = {
 };
 
 const BookCaseDemo = () => {
+	const [isMobile, setIsMobile] = useState(false);
+	const [windowWidth, setWindowWidth] = useState(0);
+	const [windowHeight, setWindowHeight] = useState(0);
+
+	useEffect(() => {
+		const setDimensions = () => {
+			const width = window.innerWidth;
+			const height = window.innerHeight;
+			setWindowWidth(width);
+			setWindowHeight(height);
+		};
+		window.addEventListener("resize", setDimensions);
+		return () => window.removeEventListener("resize", setDimensions);
+	}, []);
+
+	useEffect(() => {
+		const detectWidthAndHeight = () => {
+			if (windowWidth < 768) {
+				setIsMobile(true);
+			} else {
+				setIsMobile(false);
+			}
+		};
+		detectWidthAndHeight();
+	}, [windowWidth, windowHeight]);
+
 	return (
-		<div className="bookcase-demo project-section">
-			<Flickity
-				className={"carousel"} // default ''
-				elementType={"div"} // default 'div'
-				options={flickityOptions} // takes flickity options {}
-				disableImagesLoaded={false} // default false
-				reloadOnUpdate // default false
-				static // default false
-			>
-				<div className="carousel-item-wrapper">
-					<img
-						className="project-images"
-						src={BookcaseContent[0].image}
-						alt="bookcase-1"
-					/>
+		<>
+			{isMobile === true ? (
+				<div className="bookcase-demo project-section">
+					<div className="project-images-mobile-view">
+						<img
+							className="project-images"
+							src={BookcaseContent[0].image}
+							alt="bookcase-1"
+						/>
+					</div>
+					<div className="project-images-mobile-view">
+						<img
+							className="project-images"
+							src={BookcaseContent[1].image}
+							alt="bookcase-2"
+						/>
+					</div>
+					<div className="project-images-mobile-view">
+						<img
+							className="project-images"
+							src={BookcaseContent[2].image}
+							alt="bookcase-3"
+						/>
+					</div>
+					<div className="project-images-mobile-view">
+						<img
+							className="project-images"
+							src={BookcaseContent[3].image}
+							alt="bookcase-4"
+						/>
+					</div>
 				</div>
-				<div className="carousel-item-wrapper">
-					<img
-						className="project-images"
-						src={BookcaseContent[1].image}
-						alt="bookcase-2"
-					/>
+			) : (
+				<div className="bookcase-demo project-section">
+					<Flickity
+						className={"carousel"} // default ''
+						elementType={"div"} // default 'div'
+						options={flickityOptions} // takes flickity options {}
+						disableImagesLoaded={false} // default false
+						reloadOnUpdate // default false
+						static // default false
+					>
+						<div className="carousel-item-wrapper">
+							<img
+								className="project-images"
+								src={BookcaseContent[0].image}
+								alt="bookcase-1"
+							/>
+						</div>
+						<div className="carousel-item-wrapper">
+							<img
+								className="project-images"
+								src={BookcaseContent[1].image}
+								alt="bookcase-2"
+							/>
+						</div>
+						<div className="carousel-item-wrapper">
+							<img
+								className="project-images"
+								src={BookcaseContent[2].image}
+								alt="bookcase-3"
+							/>
+						</div>
+						<div className="carousel-item-wrapper">
+							<img
+								className="project-images"
+								src={BookcaseContent[3].image}
+								alt="bookcase-4"
+							/>
+						</div>
+					</Flickity>
 				</div>
-				<div className="carousel-item-wrapper">
-					<img
-						className="project-images"
-						src={BookcaseContent[2].image}
-						alt="bookcase-3"
-					/>
-				</div>
-				<div className="carousel-item-wrapper">
-					<img
-						className="project-images"
-						src={BookcaseContent[3].image}
-						alt="bookcase-4"
-					/>
-				</div>
-			</Flickity>
-		</div>
+			)}
+		</>
 	);
 };
 
--- src/components/DocketeerDemo.jsx
+++ src/components/DocketeerDemo.jsx
@@ -7,14 +7,7 @@ const flickityOptions = {
 };
 const DocketeerDemo = () => {
 	return (
-		<Flickity
-			className={"carousel"} // default ''
-			elementType={"div"} // default 'div'
-			options={flickityOptions} // takes flickity options {}
-			disableImagesLoaded={false} // default false
-			reloadOnUpdate // default false
-			static // default false
-		>
+		<div>
 			<div className="carousel-item-wrapper">
 				<img
 					className="project-images"
@@ -81,7 +74,82 @@ const DocketeerDemo = () => {
 				</div>
 				<script src="https://player.vimeo.com/api/player.js"></script>
 			</div>
-		</Flickity>
+		</div>
+		// <Flickity
+		// 	className={"carousel"} // default ''
+		// 	elementType={"div"} // default 'div'
+		// 	options={flickityOptions} // takes flickity options {}
+		// 	disableImagesLoaded={false} // default false
+		// 	reloadOnUpdate // default false
+		// 	static // default false
+		// >
+		// 	<div className="carousel-item-wrapper">
+		// 		<img
+		// 			className="project-images"
+		// 			src={DocketeerContent[0].image}
+		// 			alt="docketeer-1"
+		// 		/>
+		// 	</div>
+		// 	<div className="carousel-item-wrapper">
+		// 		<img
+		// 			className="project-images"
+		// 			src={DocketeerContent[1].image}
+		// 			alt="docketeer-2"
+		// 		/>
+		// 	</div>
+		// 	<div className="carousel-item-wrapper">
+		// 		<img
+		// 			className="project-images"
+		// 			src={DocketeerContent[2].image}
+		// 			alt="docketeer-3"
+		// 		/>
+		// 	</div>
+		// 	<div className="carousel-item-wrapper">
+		// 		<img
+		// 			className="project-images"
+		// 			src={DocketeerContent[3].image}
+		// 			alt="docketeer-4"
+		// 		/>
+		// 	</div>
+		// 	<div className="carousel-item-wrapper">
+		// 		<img
+		// 			className="project-images"
+		// 			src={DocketeerContent[4].image}
+		// 			alt="docketeer-5"
+		// 		/>
+		// 	</div>
+		// 	<div className="carousel-item-wrapper">
+		// 		<img
+		// 			className="project-images"
+		// 			src={DocketeerContent[5].image}
+		// 			alt="docketeer-6"
+		// 		/>
+		// 	</div>
+		// 	<div className="carousel-item-wrapper">
+		// 		<img
+		// 			className="project-images"
+		// 			src={DocketeerContent[6].image}
+		// 			alt="docketeer-7"
+		// 		/>
+		// 	</div>
+		// 	<div className="carousel-item-wrapper vimeo-wrapper">
+		// 		<div
+		// 			style={{
+		// 				width: "100%",
+		// 				justifyContent: "center",
+		// 				padding: "0 0 0 0",
+		// 				position: "relative",
+		// 			}}
+		// 		>
+		// 			<iframe
+		// 				src="https://player.vimeo.com/video/851975342?badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479"
+		// 				allow="autoplay; fullscreen; picture-in-picture"
+		// 				title="Docketeer 9.0 - Metrics Display Demo"
+		// 			></iframe>
+		// 		</div>
+		// 		<script src="https://player.vimeo.com/api/player.js"></script>
+		// 	</div>
+		// </Flickity>
 	);
 };
 export default DocketeerDemo;
