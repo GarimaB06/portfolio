3f7f234f11f9e724c85ec2026e9dc1b58dc6989f
GarimaB06 <marigolddancestudio@gmail.com>
2024-08-01 13:53:07 -0400

    code cleanup

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,9 @@
-// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist',  // Ensure this matches your workflow
+    outDir: 'dist',  
   },
 })
